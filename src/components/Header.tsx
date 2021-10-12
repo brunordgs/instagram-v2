7f3b7e2dfb44514d97c95dc5e3aa11f959d00eb6
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-10-12 16:38:01 -0300

    Finish header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,15 +11,15 @@ import { HomeIcon } from '@heroicons/react/solid';
 
 export default function Header() {
 	return (
-		<>
+		<div className="shadow-sm border-b bg-white sticky top-0 z-50">
 			<div className="flex justify-between max-w-6xl mx-5 xl:mx-auto">
 				{/* Left */}
 				<div className="relative hidden lg:inline-grid w-24 cursor-pointer">
-					<Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain" />
+					<Image src="/instagram-logo.png" layout="fill" objectFit="contain" />
 				</div>
 
 				<div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
-					<Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain" />
+					<Image src="/insta-logo.png" layout="fill" objectFit="contain" />
 				</div>
 
 				{/* Middle - Search input field */}
@@ -38,10 +38,25 @@ export default function Header() {
 
 				{/* Right */}
 				<div className="flex items-center justify-end space-x-4">
-					<HomeIcon className="w-10 h-10" />
+					<HomeIcon className="navBtn" />
 					<MenuIcon className="h-6 md:hidden cursor-pointer" />
+					<div className="relative navBtn">
+						<PaperAirplaneIcon className="navBtn rotate-45" />
+						<div className="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">
+							3
+						</div>
+					</div>
+					<PlusCircleIcon className="navBtn" />
+					<UserGroupIcon className="navBtn" />
+					<HeartIcon className="navBtn" />
+
+					<img
+						src="https://github.com/brunordgs.png"
+						alt="Bruno's profile picture"
+						className="h-7 rounded-full cursor-pointer"
+					/>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
