7905697d630d668f4452ed6c49f62c8a69b63ea8
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-10-13 08:26:42 -0300

    Add sign in

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,14 +1,29 @@
 import { GetServerSideProps } from 'next';
 import { getProviders, signIn } from 'next-auth/react';
+import Header from '../../components/Header';
 
 export default function Signin({ providers }) {
 	return (
 		<>
-			{Object.values(providers).map((provider) => (
-				<div key={provider.name}>
-					<button onClick={() => signIn(provider.id)}>Sign in with {provider.name}</button>
+			<Header />
+
+			<div className="flex flex-col items-center justify-center min-h-screen py-2 md:-mt-24 px-14 text-center">
+				<img src="/instagram-logo.png" alt="Instagram icon" className="w-80" />
+				<p className="font-xs italic">This is not REAL app, is built for study purposes only</p>
+
+				<div className="mt-40">
+					{Object.values(providers).map((provider) => (
+						<div key={provider.name}>
+							<button
+								onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+								className="p-3 bg-blue-500 rounded-lg text-white"
+							>
+								Sign in with {provider.name}
+							</button>
+						</div>
+					))}
 				</div>
-			))}
+			</div>
 		</>
 	);
 }
