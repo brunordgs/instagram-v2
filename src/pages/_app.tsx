d80b087685c91e7616df91f9fbe6ebe3af5421ae
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-10-16 12:53:22 -0300

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/global.css';
 import { SessionProvider } from 'next-auth/react';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<SessionProvider session={session}>
 			<RecoilRoot>
+				<Head>
+					<title>Instagram 2.0</title>
+					<link rel="icon" type="image/png" href="/static/instagram-icon.png" />
+				</Head>
 				<Component {...pageProps} />
 			</RecoilRoot>
 		</SessionProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,10 @@
-import Head from 'next/head';
-import Header from '../components/ui/Header';
 import Feed from '../components/Feed';
+import Header from '../components/ui/Header';
 import Modal from '../components/ui/Modal';
 
 export default function Home() {
 	return (
 		<div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
-			<Head>
-				<title>Instagram 2.0</title>
-				<link rel="icon" type="image/png" href="/instagram-icon.png" />
-			</Head>
-
 			<Modal />
 			<Header />
 			<Feed />
